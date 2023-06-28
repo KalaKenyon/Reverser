386175345e46036777f852f8a222e568c2c7a71b
KalaKenyon <100374253+KalaKenyon@users.noreply.github.com>
2023-06-28 18:21:47 -0400

    Add files via upload

---
+++ Reverser.py
@@ -0,0 +1,7 @@
+def reverse_token(token):
+    reversed_token = token[::-1]
+    return reversed_token
+
+input_token = "ENTERTOKENHERE"
+reversed_token = reverse_token(input_token)
+print("Reversed token:", reversed_token)
